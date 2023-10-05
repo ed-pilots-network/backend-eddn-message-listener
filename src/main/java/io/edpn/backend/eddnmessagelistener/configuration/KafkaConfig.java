f0095f88364fd615790515c3f39667c85e6bca1e
pveeckhout <vaneeckhout.pieter@gmail.com>
2023-10-06 00:37:46 +0200

    #10 - implementing a kafka stream processor to split journal into topics based on which event it describes

--- src/main/java/io/edpn/backend/eddnmessagelistener/configuration/KafkaConfig.java
+++ src/main/java/io/edpn/backend/eddnmessagelistener/configuration/KafkaConfig.java
@@ -1,26 +1,29 @@
 package io.edpn.backend.eddnmessagelistener.configuration;
 
-
+import io.edpn.backend.eddnmessagelistener.domain.KafkaStreamProperties;
 import io.edpn.backend.eddnmessagelistener.infrastructure.kafka.KafkaTopicHandler;
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class KafkaConfig {
 
-    @Value(value = "${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
     @Bean
-    public AdminClient kafkaAdminClient() {
+    public AdminClient kafkaAdminClient(@Value(value = "${spring.kafka.bootstrap-servers}") String bootstrapServers) {
         Map<String, Object> configs = new HashMap<>();
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         return AdminClient.create(configs);
+    }
+
+    @Bean(name = "eventRouterKafkaStreamProperties")
+    public KafkaStreamProperties kafkaStreamProperties(@Value(value = "${spring.kafka.bootstrap-servers}") String bootstrapServers) {
+        return new KafkaStreamProperties(bootstrapServers, "journal-event-router-app");
     }
 
     @Bean
